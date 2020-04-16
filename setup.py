642448ecb462c4bd6f832648b3985c51eaf8a0ec
Rafael Hidalgo <rhidalgo@thedataage.com>
2020-04-16 15:12:31 -0600

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'libcorepy',
+  packages = ['utility'], 
+  version = '0.01',
+  description = 'Library based on the gocore libcore library',
+  author = 'Rafael Antonio Hidalgo',
+  author_email = 'rafael180496@gmail.com',
+  url = 'https://github.com/rafael180496/LIBCOREPY', # use the URL to the github repo
+  download_url = 'https://github.com/rafael180496/LIBCOREPY/tarball/0.01',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
