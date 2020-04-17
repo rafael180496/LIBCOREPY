5688888473b721f31520a60df9486f8f2ef722b7
Rafael Hidalgo <rhidalgo@thedataage.com>
2020-04-16 20:37:50 -0600

    Correciones

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from distutils.core import setup
 setup(
   name = 'libcorepy',
   packages = ['utility'], 
+  license='MIT',
   version = '0.01',
   description = 'Library based on the gocore libcore library',
   author = 'Rafael Antonio Hidalgo',
   author_email = 'rafael180496@gmail.com',
   url = 'https://github.com/rafael180496/LIBCOREPY', # use the URL to the github repo
   download_url = 'https://github.com/rafael180496/LIBCOREPY/tarball/0.01',
-  keywords = ['testing', 'logging', 'example'],
-  classifiers = ['Programming Language :: Python',  # Clasificadores de compatibilidad con versiones de Python para tu paqeute
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.3',
+  keywords = 'test example develop',
+  classifiers = ['Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
