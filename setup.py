0cf640543e03a5c4644372bbfa53b03a3d197fbc
Rafael Hidalgo <rhidalgo@thedataage.com>
2020-04-20 16:20:55 -0600

    cambios de setup

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ from setuptools import setup,find_packages
 setup(
   name = 'libcorepy',
   packages = find_packages(), 
-  install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+  install_requires=["pycrypto"],
   license='MIT',
-  version = '0.0.5',
+  version = '0.0.6',
   description = 'Library based on the gocore libcore library',
   author = 'Rafael Antonio Hidalgo',
   author_email = 'rafael180496@gmail.com',
